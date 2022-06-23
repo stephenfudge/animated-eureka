9bc4412abce039cd849a9d4d590101bf1562d849
Stephen Fudge <sfudge@gmail.com>
2022-06-22 21:36:57 -0400

    updated some routes, models and server.js files

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,13 +7,67 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+     ],
+     include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+        through: ProductTag,
+        as: 'tags'
+      }
+     ]
+  })
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+    ],
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Category,
+      attributes: ['id', 'product_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name'],
+      through: ProductTag,
+      as: 'tags'
+    }
+  ]
+  }).then(productData => res.json(productData))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -25,7 +79,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tag_Ids
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -50,7 +110,14 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update(    
+    {
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tag_Ids: req.body.tag_Ids
+  }, {
     where: {
       id: req.params.id,
     },
@@ -91,6 +158,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(productData => {
+    if(!productData){
+      res.status(400).json({ message: 'No Product found with that ID' });
+      return;
+    }
+    res.json(productData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
